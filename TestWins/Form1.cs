80d37585cb123b1b004a3f4d7ad2e3c566814778
Montealto <saludarace99@gmail.com>
2026-03-29 10:45:34 +0800

    Form1.cs

--- TestWins/Form1.cs
+++ TestWins/Form1.cs
@@ -1,43 +1,82 @@
-using System.Drawing.Text;
-using TestWins.Controller;
-
-namespace TestWins;
-
-public partial class Form1 : Form
-{
-    //business
-
-    private readonly StudentController controller = new StudentController();
-    public Form1()
+private void btnAdd_Click(object sender, EventArgs e)
     {
-        InitializeComponent();
-        loadData();
-    }
+        try
+        {
+            var student = new Student
+            {
+                Name = txtName.Text,
+                Age = int.Parse(txtAge.Text)
+            };
+            controller.add(student);
+            loadData();
+            ClearFields();
 
-    private void loadData()
-    {
-        dataGridView1.DataSource = controller.getAll();
-    }
-
-    private void btnAdd_Click(object sender, EventArgs e)
-    {
-
+            MessageBox.Show("Student added successfully!");
+        }
+        catch(Exception e)
+        {
+            MessageBox.Show("Error: " + ex.Message);
+        }
     }
 
     private void btnUpdate_Click(object sender, EventArgs e)
     {
+        try
+        {
+            var student = new Student
+            {
+                id = int.Parse(txtId.Text),
+                Name = txtName.Text,
+                Age = int.Parse(txtAge.Text)
+            };
+            controller.update(student);
+            loadData();
+            ClearFields();
 
+            MessageBox.Show("Student updated successfully!");
+        }
+        catch(Exception e)
+        {
+            MessageBox.Show("Erro: " + ex.Message);
+        }
     }
 
     private void btnDelete_Click(object sender, EventArgs e)
     {
+        try
+        {
+            int id = int.Parse(txtId.Text);
 
+            var confirm = MessageBox.Show(
+            "Are you sure you want to delete this student?",
+            "Confirm Delete",
+            MessageBoxButtons.YesNo
+            );
 
+            if(confirm == DialogResult.Yes)
+            {
+                controller.delete(id);
+                loadData();
+                ClearFields();
 
+                MessageBox.Show("Student deleted successfully!");
+            }
+        }
+        catch(Exception e)
+        {
+            MessageBox.Show("Error: " + ex.Message);
+        }
     }
 
     private void dataGridView1_CellClick(object sender, EventArgs e)
     {
+        if(e.RowIndex >= 0)
+        {
+            DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
 
+            txtId.Text = row.Cells["Id"].Value.ToString();
+            txtName.Text = row.Cells["Name"].Value.ToString();
+            txtAge.Text = row.Cells["Age"].Value.ToString();
+        }
     }
 }
